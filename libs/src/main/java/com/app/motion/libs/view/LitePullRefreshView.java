9f1e631a0c85c1599129233966de4c4c8fb39bdd
joe.wang <wx104@126.com>
2018-04-02 23:21:27 +0800

    first commit

---
+++ libs/src/main/java/com/app/motion/libs/view/LitePullRefreshView.java
@@ -0,0 +1,25 @@
+package com.app.motion.libs.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class LitePullRefreshView extends ViewGroup {
+
+    public LitePullRefreshView(Context context) {
+        super(context);
+    }
+
+    public LitePullRefreshView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LitePullRefreshView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
